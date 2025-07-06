17ab3bdb8513d98db9bbe35ea00ca38b760ddcf3
Yevhen <devsaihak@gmail.com>
2025-07-06 20:22:02 +0300

    add app header with router links and mobile-friendly menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatToolbar } from '@angular/material/toolbar';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MATERIAL_IMPORTS } from './shared/material/material';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbar],
+  imports: [
+    RouterOutlet,
+    MatToolbar,
+    MatMenu,
+    MATERIAL_IMPORTS,
+    MatMenuTrigger,
+    RouterLink,
+    RouterLinkActive,
+    MatMenuItem,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
